d430cacc81a08dbf9afe4e22a4d0086cd6abc327
akolatch <aaron.kolatch@gmail.com>
2021-02-13 16:01:57 -0500

    added style components

---
+++ client/src/components/styledElements/FormButton.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors, font } from './styleGuid';
+
+const Button = styled.button`
+  font-family: ${font.mainFont};
+  font-size: 16px;
+  font-weight: bold;
+  color: ${colors.white};
+  background-color: ${(props) =>
+    props.color ? props.color : colors.clementine};
+  border: none;
+  border-radius: 20px;
+  width: 160px;
+  height: 40px;
+  margin: ${(props) => (props.margin ? props.margin : 0)};
+
+  &:active {
+    background-color: ${(props) =>
+      props.color ? colors.grape : colors.carrot};
+  }
+`;
+
+const FormButton = ({
+  children,
+  onClick,
+  color,
+  margin,
+  type,
+  disabled = false,
+  className,
+  id,
+}) => (
+  <Button
+    id={id}
+    type={type}
+    margin={margin}
+    color={color}
+    onClick={onClick}
+    disabled={disabled}
+    className={className}
+  >
+    {children}
+  </Button>
+);
+
+export default FormButton;
