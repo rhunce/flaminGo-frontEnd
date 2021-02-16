b10352fa4767d196660e15883f26d2ec5f02ade5
blargeth <albertkchua@gmail.com>
2021-02-15 21:17:19 -0500

    styling for reservation search and creates hooks for name and reservation id

--- client/src/components/frontDesk/checkIn/01searchForReservations.jsx
+++ client/src/components/frontDesk/checkIn/01searchForReservations.jsx
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import FormButton from '../../styledElements/FormButton';
 import HalfRoundDiv from '../../styledElements/HalfRoundDiv';
 import InputTypeText from '../../styledElements/InputTypeText';
+import ReservationResults from './02reservationResults';
 
 
 
 const SearchForReservations = () => {
+    const [page, setPage] = useState(0);
+    const [name, setName] = useState('');
+    const [resId, setResId] = useState('');
+    const addPage = () => {
+        setPage((prevPage) => (prevPage + 1))
+    };
+    const updateName = (e) => {
+        setName(e.target.value)
+    }
+
+    const updateResId = (e) => {
+        setResId(e.target.value)
+    }
+
+    // useEffect(() => {
+    //     console.log('name changed')
+    // }, [name])
+
+
+
+    let nameValidate = null;
+    if (name === "blob") {
+        nameValidate = <div> your input is a blob </div>
+    }
+
+    if (page === 1){
+        return <ReservationResults name={name} resId={resId}/>
+    }
     return (
         <HalfRoundDiv className="mainBox">
                 <div className="checkInTitleDiv"> Check-In Search for Reservations</div>
-                <label for="checkInGuestName"> hi</label>
-                <InputTypeText id="checkInGuestName"/>
-                <FormButton backgroundColor="berry" className="searchButtonPlacement"> Search </FormButton>
+                <div className="centeredInputBars">
+                    <label className="checkinLabels"> Name</label> <br/>
+                    <InputTypeText id="checkInGuestName" width={'100%'} value={name} onChange={updateName}/> 
+                    {nameValidate}
+
+                    <label className="checkinLabels"> Reservation ID</label> <br/>
+                    <InputTypeText id="reservation" width={'100%'} value={resId} onChange={updateResId}/>
+                </div>
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <FormButton 
+                backgroundColor="berry" 
+                className="searchButtonPlacement"
+                onClick={(e) => { console.log('hi'); addPage() }}
+                margin={'0 auto'}> Search </FormButton>
+                </div>
         </HalfRoundDiv>
     )
 }
--- client/src/components/frontDesk/checkIn/02reservationResults.jsx
+++ client/src/components/frontDesk/checkIn/02reservationResults.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import HalfRoundDiv from '../../styledElements/HalfRoundDiv';
 
-const ReservationResults = () => {
+
+const ReservationResults = ({name, resId}) => {
     return (
         <HalfRoundDiv className="mainBox">
             <div className="insidePurpleThing">
                 <div> reservation Number | Guest Name | DD/MM - DD/MM</div>
+                Search results for: {name} {resId}
             </div>
         </HalfRoundDiv>
     )
 }
-
 export default ReservationResults;
