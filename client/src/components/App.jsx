3e25a1a1173d33ade89617fa3cc4cf0a0972ff78
Jimmy Ko <ko.jimmy09@gmail.com>
2021-02-15 22:18:18 -0500

    built form to create booking with styled components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import TextAreaForm from './styledElements/TextAreaForm.jsx';
 import AddTaskForm from './maintenance/AddTaskForm.jsx';
+import CreateBookingForm from './maintenance/CreateBookingForm.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,6 +23,7 @@ class App extends React.Component {
         <h1> {this.props.projectName} </h1>
         <TextAreaForm />
         <AddTaskForm/>
+        <CreateBookingForm/>
       </div>
     );
   }
