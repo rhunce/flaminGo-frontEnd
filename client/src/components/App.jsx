647575a6cf5e878c853cd72dfb232c9da1237070
Jimmy Ko <ko.jimmy09@gmail.com>
2021-02-16 13:22:09 -0500

    resolved conflicts from dev

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-<<<<<<< HEAD
-import TextAreaForm from './styledElements/TextAreaForm.jsx';
-import AddTaskForm from './maintenance/AddTaskForm.jsx';
-=======
 import TextAreaForm from './styledElements/TextAreaForm.jsx'
 import MainHeader from './GlobalComponents/Header.jsx'
 import Landing from './landingPage/MainLanding';
->>>>>>> dev
 
 class App extends React.Component {
   constructor(props) {
@@ -25,19 +20,10 @@ class App extends React.Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <div>
-        HrNYC34 PROJECT
-        <h1> {this.props.projectName} </h1>
-        <TextAreaForm />
-        <AddTaskForm/>
-      </div>
-=======
       <main className='main'>
         <MainHeader />
         <Landing />
       </main>
->>>>>>> dev
     );
   }
 }
