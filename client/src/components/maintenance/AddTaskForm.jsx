1018448ce1774de4d3c875ffdcb9d0cd9f999c41
Jimmy Ko <ko.jimmy09@gmail.com>
2021-02-16 02:21:39 -0500

    converted AddTaskForm to class component, added state and form submit handlers

--- client/src/components/maintenance/AddTaskForm.jsx
+++ client/src/components/maintenance/AddTaskForm.jsx
@@ -4,30 +4,92 @@ import HalfRoundDiv from '../styledElements/HalfRoundDiv.jsx';
 import InputTypeText from '../styledElements/InputTypeText.jsx';
 import InputTypeRadio from '../styledElements/InputTypeRadio.jsx';
 import TextAreaForm from '../styledElements/TextAreaForm.jsx';
-import {gradient} from '../styledElements/styleGuid.js';
+import { gradient } from '../styledElements/styleGuid.js';
+import axios from 'axios';
 
-let AddTaskForm = () => {
-  return (
-    <div>
-      <HalfRoundDiv className={'addTask'}>
-        <h1 className="formTitle">Add a Task</h1>
-        <h2 className="taskInputTitle">Task Title</h2>
-        <InputTypeText className="taskInput" placeholder={'Task Title'}/>
-        <h2 className="roomInputTitle">Room Number</h2>
-        <InputTypeText className="roomInput" placeholder={'Room Number'}/>
-        <h2 className="textAreaTitle">Task Description</h2>
-        <TextAreaForm className="textArea" placeholder={'Task Description'}/>
-        <h2 className="radioTitle">Department</h2>
-        <form className="ticketType">
-          <label for="Housekeeping"><input name='task' type="radio" value="Housekeeping" id="Housekeeping"/>Housekeeping</label>
-          <label for="Maintenance"><input name='task' type="radio" value="Maintenance" id="Maintenance"/>Maintenance</label>
-        </form>
-        <FormButton className="submitButton">
-          Submit
-        </FormButton>
-      </HalfRoundDiv>
-    </div>
-  );
-};
+class AddTaskForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskTitle: '',
+      roomNumber: 0,
+      taskDescription: '',
+      taskType: ''
+    };
+    this.inputTaskTitle = this.inputTaskTitle.bind(this);
+    this.inputRoom = this.inputRoom.bind(this);
+    this.inputDescription = this.inputDescription.bind(this);
+    this.selectType = this.selectType.bind(this);
+    this.submitAddTaskForm = this.submitAddTaskForm.bind(this);
+  }
+
+  inputTaskTitle(event) {
+    event.preventDefault();
+    this.setState({taskTitle: event.target.value});
+  }
+
+  inputRoom(event) {
+    event.preventDefault();
+    this.setState({roomNumber: event.target.value});
+  }
+
+  inputDescription(event) {
+    event.preventDefault();
+    this.setState({taskDescription: event.target.value});
+  }
+
+  selectType(event) {
+    event.preventDefault();
+    this.setState({taskType: event.target.value});
+  }
+
+  submitAddTaskForm(event) {
+    event.preventDefault();
+    //from API docs: POST '/tasks' to add a new task
+    axios.post('/tasks', this.state)
+      .then((results) => { console.log('Form successfully submitted'); })
+      .catch((error) => { console.log(error); });
+  }
+
+  render() {
+    return (
+      <div>
+        <HalfRoundDiv className={'addTask'}>
+          <h1 className="formTitle">Add a Task</h1>
+          <h2 className="taskInputTitle">Task Title</h2>
+          <InputTypeText onChange={this.inputTaskTitle} className="taskInput" placeholder={'Task Title'} />
+          <h2 className="roomInputTitle">Room Number</h2>
+          <InputTypeText onChange={this.inputRoom} className="roomInput" placeholder={'Room Number'} />
+          <h2 className="textAreaTitle">Task Description</h2>
+          <TextAreaForm onChange={this.inputDescription} className="textArea" placeholder={'Task Description'} />
+          <h2 className="radioTitle">Department</h2>
+          <form className="ticketType">
+            <label for="Housekeeping">
+              <input
+                onClick={this.selectType}
+                name="task"
+                type="radio"
+                value="Housekeeping"
+                id="Housekeeping"
+              />
+              Housekeeping
+            </label>
+            <label for="Maintenance">
+              <input
+                onClick={this.selectType}
+                name="task"
+                type="radio"
+                value="Maintenance"
+                id="Maintenance"
+              />
+              Maintenance
+            </label>
+          </form>
+          <FormButton onClick={this.submitAddTaskForm} className="submitButton">Submit</FormButton>
+        </HalfRoundDiv>
+      </div>
+    );
+  }
+}
 
 export default AddTaskForm;
