e1166b33153e698e4a08b5ba6b6e7f4d62b85644
dd0sxx <contacttheotelonis@gmail.com>
2021-02-16 13:17:49 -0500

    login button is working, user is being authenticated

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import '../dist/styles/index.scss';
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App projectName="flaminGo" />, document.getElementById('app'));
+ReactDOM.render(
+    <Auth0Provider
+    domain="flamingo-tonhil.us.auth0.com"
+    clientId="8f1Js3pd0s5GgPVkZfjcqNP0C5xYT6uO"
+    redirectUri={window.location.origin}
+  >
+        <App projectName="flaminGo" />
+    </Auth0Provider>
+, document.getElementById('app'));
