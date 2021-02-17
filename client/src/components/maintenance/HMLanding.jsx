232aaf46dbcfeb89f1bdedb4fffd0a2d9784bb29
Jimmy Ko <ko.jimmy09@gmail.com>
2021-02-17 14:30:49 -0500

    inserted user id from context into add task form

--- client/src/components/maintenance/HMLanding.jsx
+++ client/src/components/maintenance/HMLanding.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
 import LandingButtons from '../landingPage/LandingButtons';
 import useChoosePath from '../landingPage/useChoosePath';
@@ -6,6 +6,7 @@ import BackArrow from '../styledElements/BackArrow';
 import ListMaster from '../GlobalComponents/ListMaster'
 import AddTaskForm from './AddTaskForm.jsx';
 import FlexCenterContainer from '../landingPage/FlexCenterContainer';
+import {MainContext} from '../landingPage/MainContext.jsx';
 // flex container
 
 const HMLanding = ({ back }) => {
@@ -16,6 +17,8 @@ const HMLanding = ({ back }) => {
     viewRooms: false,
     landing: true,
   });
+
+  const {id} = useContext(MainContext);
 
   //set up for transition to room list page
   const [backColor, setBackColor] = useState('white');
@@ -66,7 +69,7 @@ const HMLanding = ({ back }) => {
           />
         ) : paths.addTask ? (
           // place holder for add task component
-          <AddTaskForm/>
+          <AddTaskForm employeeId={id}/>
         ) : (
           <FlexCenterContainer>
             <LandingButtons routs={routs} />
