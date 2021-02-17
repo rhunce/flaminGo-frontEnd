6138eaf4f5de618ca0cda1897e06b52cc0bbf52d
akolatch <aaron.kolatch@gmail.com>
2021-02-17 16:56:02 -0500

    minor css challenge

--- client/src/components/landingPage/FlexCenterContainer.jsx
+++ client/src/components/landingPage/FlexCenterContainer.jsx
@@ -4,9 +4,6 @@ const FlexCenterContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  /* width: 100%;
-  height: 100%;
-  position: absolute; */
   margin-top: ${(props) => (props.marginTop ? props.marginTop : 0)};
 `;
 
