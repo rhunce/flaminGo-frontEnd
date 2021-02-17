521158316d28c373611df5916b18b7d9c8d32078
Jimmy Ko <ko.jimmy09@gmail.com>
2021-02-17 10:31:36 -0500

    third page of Create Booking form add guest names

---
+++ client/src/components/frontDesk/GuestInfo.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import FormButton from "../styledElements/FormButton.jsx";
+import HalfRoundDiv from "../styledElements/HalfRoundDiv.jsx";
+import InputTypeText from "../styledElements/InputTypeText.jsx";
+
+let GuestInfo = (props) => {
+  return (
+    <HalfRoundDiv>
+      <form>
+        <h2>First Name</h2>
+        <InputTypeText/>
+        <h2>Last Name</h2>
+        <InputTypeText/>
+      </form>
+      <h2>Additional Guests</h2>
+      <form>
+        <h2>Guest 2</h2>
+        <InputTypeText/>
+        <InputTypeText/>
+        <h2>Guest 3</h2>
+        <InputTypeText/>
+        <InputTypeText/>
+        <h2>Guest 4</h2>
+        <InputTypeText/>
+        <InputTypeText/>
+      </form>
+      <FormButton onClick={props.goToNext} id={'guestInfo'}>
+        Confirm Booking
+      </FormButton>
+    </HalfRoundDiv>
+  );
+};
+
+export default GuestInfo;
