008b37d9bd7920a01c8105ec71174edb23090eab
akolatch <aaron.kolatch@gmail.com>
2021-02-17 15:09:43 -0500

    minor css fix

--- client/src/components/landingPage/FlexCenterContainer.jsx
+++ client/src/components/landingPage/FlexCenterContainer.jsx
@@ -4,7 +4,10 @@ const FlexCenterContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  /* width: 100%;
   height: 100%;
+  position: absolute; */
+  margin-top: ${(props) => (props.marginTop ? props.marginTop : 0)};
 `;
 
 export default FlexCenterContainer;
