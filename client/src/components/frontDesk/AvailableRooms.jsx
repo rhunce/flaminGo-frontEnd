a3024d7cc28be7f73c35352be4a6a541446349f2
Jimmy Ko <ko.jimmy09@gmail.com>
2021-02-17 10:32:23 -0500

    second page of Create Booking Form to select from list of available rooms

---
+++ client/src/components/frontDesk/AvailableRooms.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { roomsData } from "../../SampleData/SampleData.js";
+import HalfRoundDiv from "../styledElements/HalfRoundDiv.jsx";
+import FormButton from '../styledElements/FormButton.jsx';
+
+let AvailableRooms = (props) => {
+  return (
+    <HalfRoundDiv>
+      {roomsData.map((room) => {
+        if (!room.isOccupied) {
+          return <div id="listEntry">
+            {room.roomNumber}
+            <FormButton onClick={props.goToNext} id={'roomList'}>
+              Select Room
+            </FormButton>
+          </div>
+        }
+      })}
+    </HalfRoundDiv>
+  );
+};
+
+export default AvailableRooms;
