a233c10f4ffab6924c260bc402bec114f9bc83cb
Jimmy Ko <ko.jimmy09@gmail.com>
2021-02-17 19:52:23 -0500

    built handler to set state with selected room ID

--- client/src/components/frontDesk/CreateBooking/AvailableRooms.jsx
+++ client/src/components/frontDesk/CreateBooking/AvailableRooms.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { roomsData } from "../../../SampleData/sampleData.js";
 import HalfRoundDiv from "../../styledElements/HalfRoundDiv.jsx";
-import FormButton from '../../styledElements/FormButton.jsx';
+import FormButton from "../../styledElements/FormButton.jsx";
+import InputTypeText from "../../styledElements/InputTypeText.jsx";
 
 let AvailableRooms = (props) => {
   return (
     <HalfRoundDiv>
       {roomsData.map((room) => {
         if (!room.isOccupied) {
-          return <div id="listEntry">
-            {room.roomNumber}
-            <FormButton onClick={props.goToNext} id={'roomList'}>
-              Select Room
-            </FormButton>
-          </div>
+          return (
+            <div id="listEntry" key={room._id}>
+              {room.roomNumber}
+              <FormButton
+                name={room._id}
+                onClick={(event) => {
+                  props.goToNext(event);
+                  props.selectRoom(event);
+                }}
+                id={"roomList"}
+              >
+                Select Room
+              </FormButton>
+            </div>
+          );
         }
       })}
     </HalfRoundDiv>
