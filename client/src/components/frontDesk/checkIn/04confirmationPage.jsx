0a4a29b17327f63d9f31656d45128a4fab1088df
blargeth <albertkchua@gmail.com>
2021-02-17 21:31:45 -0500

    styling changes

--- client/src/components/frontDesk/checkIn/04confirmationPage.jsx
+++ client/src/components/frontDesk/checkIn/04confirmationPage.jsx
@@ -11,16 +11,39 @@ const ConfirmationPage = (props) => {
         >
             <div>
             <div className="checkInTitleDiv"> Confirmation Page</div>
-                <div> 
-                    Booking guest: {props.selectedReservation.bookingGuest} <br/>
-                    Check in date: {props.selectedReservation.checkIn} <br/>
-                    Check out date: {props.selectedReservation.checkOut} <br/>
-                    Room type: {props.selectedReservation.roomType} <br/>
-                    ...Other info ...
-                    {/* {props.selectedReservation.name} <br/>
-                    {props.selectedReservation.name} <br/>
-                    {props.selectedReservation.name} <br/> */}
-                </div>
+      
+                    <div className="tables">
+                    <div className="headings"><b>RESERVATION INFORMATION</b></div>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td>NAME</td>
+                            <td>{props.selectedReservation.bookingGuest.toUpperCase()}</td>
+                        </tr>
+                        <tr>
+                            <td>ROOM NUMBER</td>
+                            <td>{props.selectedReservation.roomNumber}</td>
+                        </tr>
+                        <tr>
+                            <td>RESERVATION ID</td>
+                            <td>{props.selectedReservation._id}</td>
+                        </tr>
+                        <tr>
+                            <td>CHECKIN DATE</td>
+                            <td>{props.selectedReservation.checkIn}</td>
+                        </tr>
+                        <tr>
+                            <td>CHECKOUT DATE</td>
+                            <td>{props.selectedReservation.checkOut}</td>
+                        </tr> 
+                        <tr>
+                            <td>ROOM TYPE</td>
+                            <td>{props.selectedReservation.roomType}</td>
+                        </tr>
+
+                        </tbody>
+                    </table>
+                    </div>
                 <FormButton 
                 className='searchButtonPlacement' 
                 onClick={(e)=> {
