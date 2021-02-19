858ae60289c8c8bdac2da1cf97c7fbbfe2bd5ca5
akolatch <aaron.kolatch@gmail.com>
2021-02-18 19:05:23 -0500

    add home button functionality to logo on header

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,16 +3,25 @@ import MainHeader from './GlobalComponents/Header.jsx';
 import Landing from './landingPage/MainLanding';
 import Login from './login/Login.jsx';
 import { useAuth0 } from '@auth0/auth0-react';
+import useChoosePath from './landingPage/useChoosePath.jsx';
 
 const App = () => {
   const { isAuthenticated, user } = useAuth0();
+
+  const [paths, setPaths] = useChoosePath({
+    management: false,
+    hm: false,
+    frontDesk: false,
+    timeSheet: false,
+    landing: true,
+  });
 
   return (
     <div>
       {isAuthenticated ? (
         <main className='main'>
-          <MainHeader />
-          <Landing user={user} />
+          <MainHeader back={() => setPaths('landing')} />
+          <Landing paths={paths} setPaths={setPaths} />
         </main>
       ) : (
         <Login />
