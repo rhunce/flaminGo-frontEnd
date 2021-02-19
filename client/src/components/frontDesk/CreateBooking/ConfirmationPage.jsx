c7fd4a731ee3be36b3f70ff93bf73d0b28175145
Jimmy Ko <ko.jimmy09@gmail.com>
2021-02-18 20:55:27 -0500

    added axios request as click handler to confirmation page

--- client/src/components/frontDesk/CreateBooking/ConfirmationPage.jsx
+++ client/src/components/frontDesk/CreateBooking/ConfirmationPage.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import FlexCenterContainer from '../../landingPage/FlexCenterContainer.jsx';
+import FormButton from '../../styledElements/FormButton.jsx';
 
 let ConfirmationPage = (props) => {
   return (
-    <ul>
-      {Object.values(props).map((prop) => {
-        return (
-          <div>
-            {prop}
-          </div>
-        )
-      })}
-    </ul>
+    <FlexCenterContainer>
+      <ul>
+        {Object.values(props).map((prop) => {
+          return (
+            <div>
+              {prop}
+            </div>
+          )
+        })}
+      </ul>
+      <FormButton onClick={() => {
+        props.submitBooking();
+        props.clickBack();
+      }}>
+        Confirm Booking
+      </FormButton>
+    </FlexCenterContainer>
   )
 }
 
