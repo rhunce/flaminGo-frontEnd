8eb8b879626625159258ecf85062110072132613
Christina Tkatchenko <christinatka@gmail.com>
2021-02-19 00:30:54 -0500

    Added a loading spinner & fixed redirect route

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,9 +4,10 @@ import Landing from './landingPage/MainLanding';
 import Login from './login/Login.jsx';
 import { useAuth0 } from '@auth0/auth0-react';
 import useChoosePath from './landingPage/useChoosePath.jsx';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const App = () => {
-  const { isAuthenticated, user } = useAuth0();
+  const { isAuthenticated, user, isLoading } = useAuth0();
 
   const [paths, setPaths] = useChoosePath({
     management: false,
@@ -15,6 +16,21 @@ const App = () => {
     timeSheet: false,
     landing: true,
   });
+
+  if (isLoading) {
+    return (
+      <div style={{
+        position: 'fixed',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <ClipLoader color="#ffffff" loading size={80} />
+      </div>
+    );
+  }
 
   return (
     <div>
--- client/src/components/login/LogoutButton.jsx
+++ client/src/components/login/LogoutButton.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import FormButton from '../styledElements/FormButton';
 
+import {REACT_APP_REDIRECT_URL} from '../../../../env/config';
+
 const LogoutButton = (authenticated) => {
   const { logout } = useAuth0();
 
   return (
-    <FormButton backgroundColor='berry' onClick={() => logout()}>
+    <FormButton backgroundColor='berry' onClick={() => logout({
+      returnTo: REACT_APP_REDIRECT_URL,
+    })}>
       Log Out
     </FormButton>
   );
